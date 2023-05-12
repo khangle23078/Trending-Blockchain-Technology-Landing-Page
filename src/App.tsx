32a7e433f9bec59e50ff4f3a79fa1882dd6fddd6
DESKTOP-D314UU8\khangle2307 <khangle2307@gmail.com>
2023-05-12 23:46:50 +0700

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="max-w-[1440px]">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+type Props = {};
+
+const Header = (props: Props) => {
+  return (
+    <header className="mt-[56px]">
+      <div className="layout-center"></div>
+    </header>
+  );
+};
+
+export default Header;
